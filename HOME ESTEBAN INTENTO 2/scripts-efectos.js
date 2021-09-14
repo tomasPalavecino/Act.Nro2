94423d2f654ae8ffc6a6fd25b78cfab20ff9a6ef
= <=>
2021-09-14 18:00:09 -0300

    Se termino el responsive a un 95% - FRONTEND TERMINADO

--- HOME ESTEBAN INTENTO 2/scripts-efectos.js
+++ HOME ESTEBAN INTENTO 2/scripts-efectos.js
@@ -17,6 +17,20 @@ window.addEventListener('scroll',function(){
     text.style.marginTop= value*1.5 + 'px';
     btn.style.marginTop= value*1.5 + 'px';
     header.style.top= value*0.5 + 'px';
+});
 
+
+
+
+
+// MENU HAMBURGUESA EFECTOS
+
+var burgerBtn=document.getElementById("menu-btn");
+var listHeader= document.getElementById("header-list");
+
+
+burgerBtn.addEventListener('click',function(){
+    burgerBtn.classList.toggle('fa-times');
+    listHeader.classList.toggle('active');
 
 });
